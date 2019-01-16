88d1abbca345ea07509fc776ccac0aad1062ab5f
swkane <samuelwkane@gmail.com>
2019-01-16 14:46:10 -0500

    working on pulling in new way of testing using the CLI while making it play nice with dom tests

--- Tests/Fizzbuzz/run.js
+++ Tests/Fizzbuzz/run.js
@@ -20,3 +20,54 @@ axios.get(url).then(response => {
     exec("rm s.js");
   });
 });
+
+// ----------------------------------------
+
+// const fs = require("fs");
+// const axios = require("axios");
+// const { exec, spawn } = require("child_process");
+// const { argv } = require("yargs");
+
+// const tempFile = "test/s.js";
+// const tempFileStream = fs.createWriteStream(tempFile);
+
+// if (argv._.length === 0) {
+//   defaultTest();
+// } else if (argv._[0].includes("github")) {
+//   const answer = /.*github.com\/([^/.]*)\/([^/.]*)[.git]?$/.exec(argv._[0]);
+//   const url = `https://raw.githubusercontent.com/${answer[1]}/${
+//     answer[2]
+//   }/master/fizzbuzz.js`;
+//   gitTest(url);
+// } else if (argv._[0].includes("gitlab")) {
+//   const url = argv._[0] + "/raw/master/fizzbuzz.js";
+//   gitTest(url);
+// } else {
+//   console.log(`invalid command line parameter: ${argv._[0]}`);
+// }
+
+// function defaultTest() {
+//   studentCode = fs.readFileSync("../../temp.js", { encoding: "utf8" });
+//   runTests(studentCode);
+// }
+
+// function gitTest(url) {
+//   axios.get(url).then(response => {
+//     runTests(response.data);
+//   });
+// }
+
+// function runTests(studentCode) {
+//   tempFileStream.write(studentCode.replace(/['"]?use strict['"]?/, ""));
+//   tempFileStream.write(
+//     "\nmodule.exports = {  fizzbuzz: (typeof fizzbuzz) === 'function' && fizzbuzz,  fizzbuzzPrime: (typeof fizzbuzzPrime) === 'function' && fizzbuzzPrime }"
+//   );
+//   spawn("mocha", ["--colors"], { stdio: "inherit" }).on("exit", function(
+//     error
+//   ) {
+//     if (error) {
+//       console.log(error);
+//     }
+//     exec(`rm ${tempFile}`);
+//   });
+// }
